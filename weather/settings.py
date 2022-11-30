3d308da90cac58895f800c570916b13f2b234434
Harsh <73024251+Harsh-2200@users.noreply.github.com>
2022-11-30 19:21:01 +0530

    frontend and api

--- weather/settings.py
+++ weather/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+apikey = config('api')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
